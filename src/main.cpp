0e2830f3f4784aa02a7a842fbfe3fdf3f29a53f4
Caleb Johnston <caleb.johnston@gmail.com>
2024-10-09 21:53:17 -0400

    Build system setup

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+#include <glm/gtc/constants.hpp>
+
+using namespace std;
+
+int main(void)
+{	
+	std::cout << "Hello Lox" << std::endl;
+
+	return 0;
+}
