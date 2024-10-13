bb9ee06cae0d0530e14a97fbbe7d5055392b5176
Caleb Johnston <caleb.johnston@gmail.com>
2024-10-12 23:18:16 -0400

    Updates through page 42

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,88 @@
+#include <stdlib.h>
 #include <string>
 #include <iostream>
+#include <fstream>
+#include <list>
 
 using namespace std;
 
-int main(void)
-{	
-	int test_val = 100;
-	int limit_val = 10;
-	while (test_val > limit_val) {
-		test_val--;
+class Token {
+	friend std::ostream& operator<<(std::ostream& os, const Token& rhs) {
+		os << "<Token>";
+		return os;
+	}
+};
+
+class Scanner {
+  public:
+  	Scanner(const std::string& src) {}
+	std::list<Token> scanTokens() const { return {}; };
+};
+
+static bool had_error = false;
+
+void report(int line, const std::string& where, const std::string& msg)
+{
+	std::cout << "[line " << line << "] Error" << where << ": " << msg << std::endl;
+	
+	had_error = true;
+}
+
+static void error(int line, const std::string& msg) 
+{
+	report(line, "", msg);
+}
+
+void run(const std::string& source)
+{
+	Scanner scanner(source);
+	std::list<Token> tokens = scanner.scanTokens();
+
+	for (Token tok : tokens) {
+		std::cout << tok << std::endl;
 	}
 
-	std::cout << "Hello Lox " << test_val << std::endl;
+	if (had_error) std::exit(65);
+}
+
+void run_file(const std::string& path)
+{
+    std::ifstream file(path, std::ios::in | std::ios::binary);
+    if (!file) {
+        std::cerr << "Error: Could not open the file " << path << std::endl;
+        return;
+    }
+
+	std::string source((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    file.close();
+
+	run(source);
+}
+
+void run_prompt() 
+{
+	std::string input;
+	while (!had_error) {
+		std::cout << "> ";
+		std::getline(std::cin, input);
+		if (input.empty()) break;
+		run(input);
+		had_error = false;
+	}
+}
+
+int main(int argc, const char *argv[])
+{
+	if (argc > 1) {
+		std::cout << "Usage: loxcc [script]" << std::endl;
+		std::exit(64);
+	}
+	else if (argc == 1) {
+		run_file(argv[0]);
+	}
+	else {
+		run_prompt();
+	}
 
 	return 0;
 }
